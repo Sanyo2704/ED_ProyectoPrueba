1c39d0b0cfd21359524ce2c8c9d3db8eadf4fb73
Sanyo2704 <rromero8703@ceuandalucia.es>
2023-11-29 20:27:37 +0100

    message

--- Proyecto3_3/src/prueba/Clase3_3A.java
+++ Proyecto3_3/src/prueba/Clase3_3A.java
@@ -4,7 +4,7 @@ public class Clase3_3A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Contenido deseado");
+		System.out.println("Contenido deseado2");
 	}
 
 }
